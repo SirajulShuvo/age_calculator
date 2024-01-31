d760bbefe9bb393b2e1a89860ff04f36f278e464
SirajMia <sirajulshuvo08@gmail.com>
2024-02-01 00:03:51 +0600

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+// function functionName(para,para2){//function body};
+// const printHelloWorld = function(para,para2){
+//     return para * para2;
+// }
+// console.log(printHelloWorld(4,5))
+
+function add () {
+    console.log(this)
+}
+add()
