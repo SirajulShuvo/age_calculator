4376524fa035dc621b5857f6164a2301a0f3e63c
SirajMia <sirajulshuvo08@gmail.com>
2024-02-05 22:04:48 +0600

    age calculator

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// function functionName(para,para2){//function body};
-// const printHelloWorld = function(para,para2){
-//     return para * para2;
-// }
-// console.log(printHelloWorld(4,5))
+function calculateAge() { 
+  let today = new Date();
+  let inputBox = document.getElementById('input-box').value;
+  let userDay = new Date(inputBox);
+  
+  let age = today.getFullYear() - userDay.getFullYear();//normal rule
+  let month = today.getMonth() - userDay.getMonth();
+  let day = today.getDate() - userDay.getDate();
 
-function add () {
-    console.log(this)
+  if((today.getMonth()) < (userDay.getMonth())){
+    age--;
+    month = month + 12
+  }
+  if((today.getDate()) < (userDay.getDate())){
+    month--;
+    day = (today.getDate()+30)-(userDay.getDate());
+  }
+
+  document.getElementById("btn1").innerHTML = `${age}`;
+  document.getElementById("btn2").innerHTML = `${month}`;
+  document.getElementById("btn3").innerHTML = `${day}`;
 }
-add()
